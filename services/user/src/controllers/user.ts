c8e57f50c020a70e5dd7b2c8b36a5ca4b917098f
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-24 02:01:27 +0530

    Added update profile pic feature

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -3,6 +3,9 @@ import User from "../model/User.js";
 import jwt from "jsonwebtoken"
 import TryCatch from "../utils/TryCatch.js";
 import type { AuthenticatedRequest } from "../middleware/isAuth.js";
+import getBuffer from "../utils/dataUri.js";
+import { v2 as cloudinary } from 'cloudinary';
+
 
 
 export const loginUser = TryCatch(async(req ,res)=>{
@@ -50,7 +53,7 @@ export const getUserProfile = TryCatch(async(req,res)=>{
 
 export const updateUser = TryCatch(async(req : AuthenticatedRequest,res)=>{
     const {name,instagram , facebook,linkedin,bio} = req.body;
-    const user = await User.findByIdAndUpdate(req.user?.id,{
+    const user = await User.findByIdAndUpdate(req.user?._id,{
         name,
         instagram,
         facebook,
@@ -69,3 +72,41 @@ export const updateUser = TryCatch(async(req : AuthenticatedRequest,res)=>{
         
     })
 });
+
+
+export const updateProfilePic = TryCatch(async(req:AuthenticatedRequest ,res)=>{
+    const file = req.file
+    if(!file){
+        res.status(400).json({
+            message:"No file to upload"
+        });
+        return;
+    }   
+    const fileBuffer = getBuffer(file)
+
+    if(!fileBuffer || !fileBuffer.content){
+        res.status(400).json({
+            message:"Failed to generate buffer",
+        });
+        return;
+    }
+    const cloud = await cloudinary.uploader.upload(fileBuffer.content,{
+        floder:"blogs",
+    });
+
+    const user = await User.findByIdAndUpdate(req.user?._id,{
+        image:cloud.secure_url
+    },{new:true});
+    
+     const token =jwt.sign({user},process.env.JWT_SEC as string, {
+        expiresIn:"5d",
+    })
+
+    res.json({
+      message:"User Profile  pic got updated",
+      token,
+      user,
+    })
+}
+  
+)
--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { getUserProfile, loginUser, myProfile, updateUser } from "../controllers/user.js";
+import { updateProfilePic,getUserProfile, loginUser, myProfile, updateUser } from "../controllers/user.js";
 import {isAuth} from "../middleware/isAuth.js"
+import uploadFile from "../middleware/multer.js";
 const router = express.Router();
 
 router.post("/login",loginUser);
 router.get("/me",isAuth,myProfile);
 router.get("/user/:id",getUserProfile);
-router.post("/user/update/:id",isAuth,updateUser);
+router.post("/user/update/",isAuth,updateUser);
+router.post("/user/update/pic",isAuth,uploadFile,updateProfilePic);
 export default router;
