90b62ce0182b2e217b6b33a3052a1b2d695ea784
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-28 17:25:05 +0530

    Redis server bug fixed

--- services/blog/src/server.ts
+++ services/blog/src/server.ts
@@ -1,13 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import blogRoutes from './routes/blog.js'
-const app= express();
+import express from "express";
+import dotenv from "dotenv";
+import { createClient } from "redis";
+import blogRoutes from "./routes/blog.js";
+
 dotenv.config();
+const app = express();
+const port = process.env.PORT || 5002;
 
-const port = process.env.PORT;
-app.use("/api/v1",blogRoutes);
-// console.log(blogRoutes);
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.use("/api/v1", blogRoutes);
+
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    connectTimeout: 10000, 
+  },
+});
+
+redisClient.on("connect", () => console.log("Connected to Redis"));
+redisClient.on("reconnecting", () => console.log("Reconnecting to Redis..."));
+redisClient.on("error", (err) => console.error(" Redis Client Error:", err));
+
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log("Redis is ready to use!");
+  } catch (err) {
+    console.error("Failed to connect to Redis:", err);
+  }
+})();
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
