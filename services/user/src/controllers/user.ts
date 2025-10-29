3964db7fc167e78edd3a9bf9adbdee36ab5c0b2a
Mahadev710 <mahadevkeshari790@gmail.com>
2025-10-30 03:51:21 +0530

    Backend completed with adding authorization with google auth and frontend begins

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -1,35 +1,55 @@
-import { Request,Response } from "express"
+
 import User from "../model/User.js";
 import jwt from "jsonwebtoken"
 import TryCatch from "../utils/TryCatch.js";
 import type { AuthenticatedRequest } from "../middleware/isAuth.js";
 import getBuffer from "../utils/dataUri.js";
 import { v2 as cloudinary } from 'cloudinary';
+import { oauth2client } from "../utils/GoogleConfig.js";
+import axios from "axios";
 
 
 
-export const loginUser = TryCatch(async(req ,res)=>{
-       //For  Dummy purpose
-       const {email,name,image} = req.body;
+export const loginUser = TryCatch(async (req, res) => {
+  const { code } = req.body;
 
-       let user= await User.findOne({email});
+  if (!code) {
+    res.status(400).json({
+      message: "Authorization code is required",
+    });
+    return;
+  }
 
-       if(!user) {
-        user= await User.create({
-            name,
-            email,
-            image
-        })
-       }
-       const token =jwt.sign({user},process.env.JWT_SEC as string,{
-        expiresIn:"5d"
-       });
-       res.status(200).json({
-        message:"Login successfully",
-        token,
-        user
-       })
-})
+  const googleRes = await oauth2client.getToken(code);
+
+  oauth2client.setCredentials(googleRes.tokens);
+
+  const userRes = await axios.get(
+    `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${googleRes.tokens.access_token}`
+  );
+  //@ts-ignore
+  const { email, name, picture } = userRes.data;
+
+  let user = await User.findOne({ email });
+
+  if (!user) {
+    user = await User.create({
+      name,
+      email,
+      image: picture,
+    });
+  }
+
+  const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+    expiresIn: "5d",
+  });
+
+  res.status(200).json({
+    message: "Login success",
+    token,
+    user,
+  });
+});
 
 
 export const myProfile = TryCatch(async(req:AuthenticatedRequest ,res)=>{
